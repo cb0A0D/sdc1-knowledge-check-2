3e9962c47e01f7af50ba7792a8ed10fa3f7c8c73
cb0a0d@gmail.com <cb0a0d@gmail.com>
2022-11-06 17:25:28 -0500

    New Project
    New Project class & gitignore update

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sdc1_knowledge_check_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
